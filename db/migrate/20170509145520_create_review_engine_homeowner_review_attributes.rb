5324c507f31fd1e8b92005cfe34b454c70c21b67
Luis Saffie <luis@homestars.com>
2017-05-09 17:22:32 -0400

    add value column to attributes

--- db/migrate/20170509145520_create_review_engine_homeowner_review_attributes.rb
+++ db/migrate/20170509145520_create_review_engine_homeowner_review_attributes.rb
@@ -3,6 +3,7 @@ class CreateReviewEngineHomeownerReviewAttributes < ActiveRecord::Migration
     create_table :review_engine_homeowner_review_attributes do |t|
       t.integer :homeowner_review_id
       t.integer :homeowner_review_key_id
+      t.string  :value
 
       t.timestamps null: false
     end
